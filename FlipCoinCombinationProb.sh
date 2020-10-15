98af2568e763ed75295ca7f99ee7745bd75625ae
Zeenat100 <siddzee100@gmail.com>
2020-10-15 00:50:36 -0700

    Single Flip Result

--- FlipCoinCombinationProb.sh
+++ FlipCoinCombinationProb.sh
@@ -1,2 +1,12 @@
 echo "Welcome to Flip Coin Combination Simulation"
+heads=1
+tails=2
+result=$(( $RANDOM % 2 +1))
+echo $result
+if [[ $result -eq $heads ]]
+then
+	echo "Heads is the winner"
+else
+	echo "Tails is the winner"
+fi
 
