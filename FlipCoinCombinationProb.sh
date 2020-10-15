386ad1957a769433e67a1e3adfe107b1da346816
Zeenat100 <siddzee100@gmail.com>
2020-10-15 02:24:12 -0700

    Add doublet Percentage

--- FlipCoinCombinationProb.sh
+++ FlipCoinCombinationProb.sh
@@ -2,6 +2,8 @@ echo "Welcome to Flip Coin Combination Simulation"
 heads=1
 tails=2
 declare -A singlet
+declare -A doublet
+number_coins=2
 echo "Enter the number of flip: "
 read countFlip
 function singlet()
@@ -11,10 +13,8 @@ function singlet()
 		result=$(( $RANDOM % 2 +1))
 		if [[ $result -eq $heads ]]
 		then
-			echo "Heads is the winner"
 			coinResult=Heads
 		else
-			echo "Tails is the winner"
 			coinResult=Tails
 
 		fi
@@ -30,6 +30,34 @@ function singletPercentage()
 		echo "The percentage of singlet[$index]=`expr "scale=2; ${singlet[$index]} * 100 / $countFlip" | bc`"
 	done
 }
+function doublet()
+{
+        for (( count=0 ; count < $countFlip ; count++ ))
+        do
+		for (( coin_num=0 ; coin_num<$number_coins ; coin_num++ ))
+                do
+			result=$(( $RANDOM % 2 +1))
+                	if [[ $result -eq $heads ]]
+                	then
+                        	coinsResult+=H
+                	else
+                        	coinsResult+=T
+
+                fi
+		done
+        	((doublet[$coinsResult]++))
+        	coinsResult=""
+        done
+}
+
+function doubletPercentage()
+{
+        for index in ${!doublet[@]}
+        do
+                echo "The percentage of doublet[$index]=`expr "scale=2; ${doublet[$index]} * 100 / $countFlip" | bc`"
+        done
+}
 singlet
 singletPercentage
-
+doublet
+doubletPercentage
