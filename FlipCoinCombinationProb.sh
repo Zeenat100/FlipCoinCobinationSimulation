a671c344413f7c1ff32dc1a9bb85de63c5dc6d4e
Zeenat100 <siddzee100@gmail.com>
2020-10-15 02:02:25 -0700

    Add Singles coin Percentage

--- FlipCoinCombinationProb.sh
+++ FlipCoinCombinationProb.sh
@@ -1,12 +1,35 @@
 echo "Welcome to Flip Coin Combination Simulation"
 heads=1
 tails=2
-result=$(( $RANDOM % 2 +1))
-echo $result
-if [[ $result -eq $heads ]]
-then
-	echo "Heads is the winner"
-else
-	echo "Tails is the winner"
-fi
+declare -A singlet
+echo "Enter the number of flip: "
+read countFlip
+function singlet()
+{
+	for (( count=0 ; count < $countFlip ; count++ ))
+	do
+		result=$(( $RANDOM % 2 +1))
+		if [[ $result -eq $heads ]]
+		then
+			echo "Heads is the winner"
+			coinResult=Heads
+		else
+			echo "Tails is the winner"
+			coinResult=Tails
+
+		fi
+	((singlet[$coinResult]++))
+	coinResult=""
+	done
+}
+
+function singletPercentage()
+{
+	for index in ${!singlet[@]}
+	do
+		echo "The percentage of singlet[$index]=`expr "scale=2; ${singlet[$index]} * 100 / $countFlip" | bc`"
+	done
+}
+singlet
+singletPercentage
 
