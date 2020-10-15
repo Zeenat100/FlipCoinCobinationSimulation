99d4a604d85594b0ab6a605647eec8a52426a439
Zeenat100 <siddzee100@gmail.com>
2020-10-15 02:42:22 -0700

    Triplet Percentage added

--- FlipCoinCombinationProb.sh
+++ FlipCoinCombinationProb.sh
@@ -3,7 +3,9 @@ heads=1
 tails=2
 declare -A singlet
 declare -A doublet
+declare -A triplet
 number_coins=2
+triplet_coins=3
 echo "Enter the number of flip: "
 read countFlip
 function singlet()
@@ -18,7 +20,7 @@ function singlet()
 			coinResult=Tails
 
 		fi
-	((singlet[$coinResult]++))
+	(( singlet[$coinResult]++))
 	coinResult=""
 	done
 }
@@ -57,7 +59,37 @@ function doubletPercentage()
                 echo "The percentage of doublet[$index]=`expr "scale=2; ${doublet[$index]} * 100 / $countFlip" | bc`"
         done
 }
+function triplet()
+{
+        for (( count=0 ; count < $countFlip ; count++ ))
+        do
+                for (( coin_num=0 ; coin_num<$triplet_coins ; coin_num++ ))
+                do
+                        result=$(( $RANDOM % 2 +1))
+                        if [[ $result -eq $heads ]]
+                        then
+                                triplet_coinsResult+=H
+                        else
+                                triplet_coinsResult+=T
+
+                fi
+                done
+                ((triplet[$triplet_coinsResult]++))
+                triplet_coinsResult=""
+        done
+}
+
+function tripletPercentage()
+{
+        for index in ${!triplet[@]}
+        do
+                echo "The percentage of triplet[$index]=`expr "scale=2; ${triplet[$index]} * 100 / $countFlip" | bc`"
+        done
+}
+
 singlet
 singletPercentage
 doublet
 doubletPercentage
+triplet
+tripletPercentage
